576848e71c911f37283acaf254b93846cdbb6722
GohUnTsuan <39488085+GohUnTsuan@users.noreply.github.com>
2021-02-13 00:59:34 +0800

    update region data, missing shanxi province data.

--- temp.R
+++ temp.R
@@ -1,3 +1,11 @@
 library(readxl)
 temp <- read_excel('./data/missing/temp.xlsx')
 save(temp, file = './data/missing/temp.csv')
+
+
+library(readxl)
+temp <- read_excel('./data/missing/temp.xlsx')
+save(temp, file = './data/missing/temp.csv')
+
+
+unique(temp$prov_name)
