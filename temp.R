fd740b2cd9cfe26508eee24c0e37412be17419d5
GohUnTsuan <39488085+GohUnTsuan@users.noreply.github.com>
2021-02-17 16:11:21 +0800

    updated and renamed region_table.rda file

--- temp.R
+++ temp.R
@@ -5,7 +5,19 @@ save(temp, file = './data/missing/temp.csv')
 
 library(readxl)
 temp <- read_excel('./data/missing/temp.xlsx')
-save(temp, file = './data/missing/temp.csv')
-
+save(temp, file = './data/missing/temp1.csv')
+write.csv(temp,file = './data/missing/full.csv')
 
 unique(temp$prov_name)
+
+region_table <- read.csv('./data/missing/full.csv')
+
+unique(full$prov_name)
+
+
+save(full,file = './data/missing/region_data_all.rds')
+test <- readRDS('region_data_all.rds')
+
+save(region_table, file = "./data/region_table.rda")
+
+unique(region_table$prov_name)
