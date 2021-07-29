b6584cb12eaf87d4984d5cf782ee32d212c8c4ac
wuwenquan <gohuntsuan@gmail.com>
2021-07-29 12:23:18 +0800

    fixed warning messages

--- R/corruption.R
+++ R/corruption.R
@@ -1,13 +1,16 @@
-#' [Filling the Title]
+#' China’s Corruption Investigations Dataset
 #'
-#' A dataset containing [Filling the description]
+#' A dataset containing information on almost 20,000 officials who were investigated during Xi Jinping’s anti-corruption campaign.
 #'
 #' @format A data frame with XXXXXX10 variables:
 #' \describe{
-#'   \item{name}{description}
-#'   \item{name}{description}
-#'   ...
+#'   \item{province}{2-digit province number}
+#'   \item{prefecture}{Prefecture name in Chinese}
+#'   \item{county}{County name in Chinese}
+#'   \item{province_id}{6-digit province number}
+#'   \item{prefecture_id}{6-digit province number}
+#'   \item{county_id}{6-digit province number}
 #' }
-#' @source \url{http://www.diamondse.info/} # link where you got the data if there is
+#' @source \url{https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/9QZRAD} # link where you got the data if there is
 #'
 "corruption"
