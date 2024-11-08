77f95ab9258bc6358a9d771c6a02e93ce55e2dae
Xinyi Ye <yexinyi98@outlook.com>
2024-11-08 16:39:19 +0800

    Population Data for Yunnan Province Cities (1986–2019) and National Cities (2020–2022)
    【更新】
    
    1.删除县级市
    
    2.更正重庆市武隆区（2017年撤县设区）
    
    3.处理重庆 、海南的错误代码
    海南省1987 属于广东行政区， 1988成为省
    重庆 96属于四川 97之后成为直辖市
    
    4.处理云南缺失人口数据
    
    5.新增20-22年全国人口数据
    
    参考资料：https://www.mca.gov.cn/n156/n186/index.html
    
    【待更新】 
    
    1.需更新以年份判断省份
    
    2.贵州人口缺失
    
    3.20-22部分市人口缺失

--- dev/cityranking.R
+++ dev/cityranking.R
@@ -1,9 +1,6 @@
 # citylevel_population
 # 目的：在sysdata.rda中增加城市规模，根据城市的人口计算出城市规模。
 # 数据：中国城市统计年鉴、中国人口统计年鉴
-rm(list = ls())
-setwd("C:/Users/amand/Documents/GitHub/regioncode_new")
-
 if (!requireNamespace("dplyr", quietly = TRUE)) {
   install.packages("dplyr")
 }
@@ -19,11 +16,22 @@ p_load("rio",
 # import---------------------------------------------------------------------------
 
 load("R/sysdata.rda")
-df_citylevel<- import("data/cityranking.xls")
+df_citylevel<- import("inst/extdata/cityranking.xls")
+yunnan<- import("inst/extdata/云南人口构成.xlsx")
 
-library(tidyverse)
+yunnan <- yunnan %>%
+  filter(!is.na(year))%>%
+  mutate(population= as.numeric(population))
+
+new<- import("inst/extdata/全国各市城镇人口构成.xlsx")
+
+new <- new %>%
+  mutate(population = as.numeric(population)) %>%
+  arrange(code, year)
+
 
 # 定义函数，根据population生成cityranking变量-----------------------------------------
+
 generate_cityranking <- function(df) {
   year <- unique(df$year)
   year_cityranking <- paste0(year, "_cityranking")
@@ -70,4 +78,70 @@ region_data <- merge(region_table,cityranking, by = "city_code")
 region_data <- select(region_data, -city_code,-city_name)
 
 save(region_data, region_table, corruption,file = "~/R/sysdata.rda")
+
+
+# 云南 -----------------------------------------------------------------------------
+
+new_cityranking <- function(df, year) {
+  year_cityranking <- paste0(year, "_cityranking")
+
+  if (year %in% 1986:2013) {
+    df[[year_cityranking]] <- case_when(
+      df$population > 100 ~ "特大城市",
+      df$population > 50 & df$population <= 100 ~ "大城市",
+      df$population > 20 & df$population <= 50 ~ "中等城市",
+      df$population <= 20 ~ "小城市",
+      TRUE ~ NA_character_
+    )
+  } else if (year > 2013) {
+    df[[year_cityranking]] <- case_when(
+      df$population > 1000 ~ "超大城市",
+      df$population > 500 & df$population <= 1000 ~ "特大城市",
+      df$population > 300 & df$population <= 500 ~ "I型大城市",
+      df$population > 100 & df$population <= 300 ~ "II型大城市",
+      df$population > 50 & df$population <= 100 ~ "中等城市",
+      df$population > 20 & df$population <= 50 ~ "I型小城市",
+      df$population <= 20 ~ "II型小城市",
+      TRUE ~ NA_character_
+    )
+  }
+
+  df <- df %>% select(-year, -population)
+  return(df)
+}
+
+
+dfs <- map(1986:2019, ~yunnan %>%
+             filter(year == .x) %>%
+             new_cityranking(., .x))
+
+yunnan_cityranking<- reduce(dfs, full_join, by = c("name"))
+
+yunnan_cityranking <- yunnan_cityranking %>%
+  select(-contains("code"))
+
+write_xlsx(yunnan_cityranking, "yunnan_cityranking.xlsx")
+
+
+# new 20-22 ----------------------------------------------------------------------------
+
+dfs <- map(2020:2022, ~new %>%
+             filter(year == .x) %>%
+             new_cityranking(., .x))
+
+
+new_cityranking<- reduce(dfs, full_join, by = c("code"))
+
+new_cityranking <- new_cityranking %>%
+  select(name,code,contains("cityrank"))
+
+region_data <- region_data %>%
+  mutate(join_key = case_when(
+    zhixiashi == TRUE ~ prov_code,
+    zhixiashi != TRUE ~ `2022_code`
+  ))
+
+region_data  <- region_data %>%
+  left_join(new_cityranking, by = c("join_key" = "code")) %>%
+  select(-"join_key")
 
